39033a8dbed4ce09e5e65c827191adbdcd78d801
Timothy Karani <kartimothy@gmail.com>
2025-01-31 18:45:50 +0300

    #2: Testing authorization done

--- app/test_main.py
+++ app/test_main.py
@@ -2,9 +2,19 @@ from fastapi.testclient import TestClient
 from models import EmbeddingModel, ModelSettings
 from sentence_transformers import SentenceTransformer
 
-from .main import app
+from .config import Settings
+from .main import app, get_settings
 
 client = TestClient(app)
+
+TOKEN = "secret_token"
+
+
+def get_settings_override():
+    return Settings(token="secret_token")
+
+
+app.dependency_overrides[get_settings] = get_settings_override
 
 
 def test_can_view_docs_through_root():
@@ -23,6 +33,7 @@ def test_can_get_embeddings():
     body = {
         "text": "That no man knows, let it suffice",
         "model": EmbeddingModel.all_mpnet_base_v2.value,
+        "token": TOKEN,
     }
 
     model = SentenceTransformer(body["model"])
@@ -41,6 +52,7 @@ def test_can_not_exceed_max_sequence_length():
     body = {
         "text": "a" * 1700,
         "model": EmbeddingModel.all_mpnet_base_v2.value,
+        "token": TOKEN,
     }
 
     response = client.post("/embeddings/", json=body)
@@ -54,3 +66,19 @@ def test_can_not_exceed_max_sequence_length():
             }
         ]
     }
+
+
+def test_can_not_get_embeddings_while_unauthorized():
+    body = {
+        "text": "a" * 1700,
+        "model": EmbeddingModel.all_mpnet_base_v2.value,
+        "token": "wrong token",
+    }
+
+    response = client.post("/embeddings/", json=body)
+    assert response.status_code == 403
+    assert response.json() == {"detail": "Forbidden"}
+
+
+def test_can_get_settings():
+    assert get_settings().token == Settings().token
