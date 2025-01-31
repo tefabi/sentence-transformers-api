667b63805ffc52cd2482abd3873be6dbc3e2f240
Timothy Karani <kartimothy@gmail.com>
2025-01-31 17:22:28 +0300

    Testing can get embeddings

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 test:
-	coverage run --source=app,models -m pytest
+	coverage run --source=app,models -m pytest -s
 
 coverage:
 	coverage report -m
--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,6 @@
 from fastapi.testclient import TestClient
+from models import EmbeddingModel, ModelSettings
+from sentence_transformers import SentenceTransformer
 
 from .main import app
 
@@ -15,3 +17,21 @@ def test_can_check_health():
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"message": "Ok"}
+
+
+def test_can_get_embeddings():
+    body = {
+        "text": "That no man knows, let it suffice",
+        "model": EmbeddingModel.all_mpnet_base_v2.value,
+    }
+
+    model = SentenceTransformer(body["model"])
+    embeddings = model.encode(body["text"]).tolist()
+
+    response = client.post("/embeddings", json=body)
+    assert response.status_code == 200
+    assert response.json() == {
+        "model": body["model"],
+        "text": body["text"],
+        "embeddings": embeddings,
+    }
