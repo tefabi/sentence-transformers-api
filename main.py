40fac28f21e6cdafad756c5535d4350bec0f68aa
Timothy Karani <kartimothy@gmail.com>
2025-01-31 15:09:58 +0300

    Typed returned data

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from sentence_transformers import SentenceTransformer
 
 app = FastAPI()
 
-@app.get('/health')
+@app.get('/health/')
 async def health():
     return {'message': "Ok"}
+
+
+class EmbeddingBody(BaseModel):
+    text: str
+
+
+class EmbeddingResponse(BaseModel):
+    text: str
+    embeddings: list[int]
+
+
+@app.post("/embeddings/")
+async def embeddings(data: EmbeddingBody) -> EmbeddingResponse:
+    model = SentenceTransformer("all-mpnet-base-v2")
+    embeddings = model.encode(data.text).tolist()
+
+    print(len(embeddings))
+    return EmbeddingResponse(text=data.text, embeddings=embeddings)
