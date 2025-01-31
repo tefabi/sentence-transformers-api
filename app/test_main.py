7134a4da675152a9ab34b3f7d973585d614ebefd
Timothy Karani <kartimothy@gmail.com>
2025-01-31 17:26:53 +0300

    Achieved 100% coverage

--- app/test_main.py
+++ app/test_main.py
@@ -35,3 +35,22 @@ def test_can_get_embeddings():
         "text": body["text"],
         "embeddings": embeddings,
     }
+
+
+def test_can_not_exceed_max_sequence_length():
+    body = {
+        "text": "a" * 1700,
+        "model": EmbeddingModel.all_mpnet_base_v2.value,
+    }
+
+    response = client.post("/embeddings/", json=body)
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "loc": "text",
+                "msg": "The 'text' field length(1700) must not be greater than 1536 characters.",
+                "type": "value_error",
+            }
+        ]
+    }
